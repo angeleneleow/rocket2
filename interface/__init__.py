85f70f966e6a46584ef28cb1f73a6ab4d2b99239
Cheuk Yin Ng <chucksys88@gmail.com>
2019-08-23 14:57:17 -0700

    Deal with pipfile conflicts

--- interface/__init__.py
+++ interface/__init__.py
@@ -1,1 +1,22 @@
 """Interface Init."""
+from slack import WebClient
+import interface.github as github
+import interface.github_app as github_app
+import interface.slack as slack
+import dependency_injector.containers as containers
+import dependency_injector.providers as providers
+
+
+class Interfaces(containers.DeclarativeContainer):
+    """All interfaces."""
+
+    github = providers.Factory(github.GithubInterface)
+    github_app_auth = providers.Factory(
+        github_app.GithubAppInterface.GithubAppAuth)
+    slack = providers.Factory(slack.Bot)
+
+
+class Clients(containers.DeclarativeContainer):
+    """All third party libraries for the interface."""
+
+    slack = providers.Factory(WebClient)
